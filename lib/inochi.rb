1fd088a4ea72e73f18a4ebe9f153b4cee1f011d8
Suraj N. Kurapati <sunaku@gmail.com>
2009-01-18 22:36:03 -0800

    specify required gem versions by major number... hopefully everyone follows the RubyGems rational versioning policy to make this work

--- lib/inochi.rb
+++ lib/inochi.rb
@@ -6,11 +6,11 @@ Inochi.init :Inochi,
   :tagline => 'Gives life to RubyGems-based software',
   :website => 'http://snk.tuxfamily.org/lib/inochi',
   :require => {
-    'rubyforge'   => '~> 1',    # for publishing gems to RubyForge
-    'mechanize'   => '0.9.0',   # for automating RAA.ruby-lang.org
-    'trollop'     => '~> 1.10', # for parsing command-line options
-    'erbook'      => '5.0.0',   # for processing the user manual
-    'launchy'     => '~> 0.3',  # for launching a web browser
-    'yard'        => nil,       # for generating API documentation
-    'addressable' => '~> 2',    # for parsing URIs properly
+    'rubyforge'   => '~> 1',  # for publishing gems to RubyForge
+    'mechanize'   => '~> 0',  # for automating web browsing
+    'trollop'     => '~> 1',  # for parsing command-line options
+    'erbook'      => '~> 5',  # for processing the user manual
+    'launchy'     => '~> 0',  # for launching a web browser
+    'yard'        => nil,     # for generating API documentation
+    'addressable' => '~> 2',  # for parsing URIs properly
   }
