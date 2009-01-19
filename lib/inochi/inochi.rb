5a0edba124cc29b1228caf5effcc9596c614a169
Suraj N. Kurapati <sunaku@gmail.com>
2009-01-18 23:48:27 -0800

    add main project executable, which generates scaffolds

--- lib/inochi/inochi.rb
+++ lib/inochi/inochi.rb
@@ -761,6 +761,39 @@ class << self
     end
   end
 
+  ##
+  # Returns the name of the main program executable, which
+  # is the same as the project name fully in lowercase.
+  #
+  def calc_program_name project_symbol
+    camel_to_snake_case(project_symbol).downcase
+  end
+
+  ##
+  # Calculates the name of the project module from the given project name.
+  #
+  def calc_project_symbol project_name
+    name = project_name.to_s.gsub(/\W+/, '_').squeeze('_').gsub(/^_|_$/, '')
+    (name[0,1].upcase + name[1..-1]).to_sym
+  end
+
+  ##
+  # Transforms the given input from CamelCase to snake_case.
+  #
+  def camel_to_snake_case input
+    input = input.to_s.dup
+
+    # handle camel case like FooBar => Foo_Bar
+    while input.gsub!(/([a-z]+)([A-Z])(\w+)/) { $1 + '_' + $2 + $3 }
+    end
+
+    # handle abbreviations like XMLParser => XML_Parser
+    while input.gsub!(/([A-Z]+)([A-Z])([a-z]+)/) { $1 + '_' + $2 + $3 }
+    end
+
+    input
+  end
+
   private
 
   ##
@@ -769,14 +802,6 @@ class << self
   #
   def first_caller_file
     caller.each {|s| !s.include? __FILE__ and s =~ /^(.*?):\d+/ and break $1 }
-  end
-
-  ##
-  # Returns the name of the main program executable, which
-  # is the same as the project name fully in lowercase.
-  #
-  def calc_program_name project_symbol
-    project_symbol.to_s.downcase
   end
 
   ##
