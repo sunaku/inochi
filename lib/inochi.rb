a46256b212c0fe562d03085d2cdde6c013f0a60f
Suraj N. Kurapati <sunaku@gmail.com>
2009-10-03 11:35:33 -0700

    Version 1.1.1

--- lib/inochi.rb
+++ lib/inochi.rb
@@ -17,25 +17,25 @@ require 'inochi/book'
 require 'inochi/util'
 
 Inochi.init :Inochi,
-  :version => '1.1.0',
-  :release => '2009-09-06',
+  :version => '1.1.1',
+  :release => '2009-10-03',
   :website => 'http://snk.tuxfamily.org/lib/inochi/',
   :tagline => 'Gives life to RubyGems-based software',
   :require => {
-    'trollop'        => '~> 1',      # for parsing command-line options
-    'launchy'        => '>= 0.3.3',  # for launching a web browser
+    'trollop'        => '~> 1',                 # for parsing command-line
+    'launchy'        => ['~> 0', '>= 0.3.3'],   # for launching a browser
   },
   :develop => {
-    'rake'           => '>= 0.8.4',
-    'rubyforge'      => '~> 2',      # for publishing gems to RubyForge
-    'mechanize'      => '~> 0',      # for automating web browsing
-    'voloko-sdoc'    => '>= 0.2.10', # for generating API documentation
-    'addressable'    => '~> 2',      # for parsing URIs properly
-    'erbook'         => '~> 7',      # for processing the user manual
-    'babelfish'      => '~> 0',      # for human language translation
-    'relevance-rcov' => nil,         # for code coverage statistics
-    'flay'           => nil,         # for code quality analysis
-    'reek'           => nil,         # for code quality analysis
-    'roodi'          => nil,         # for code quality analysis
-    'ZenTest'        => '~> 4',      # for the `multiruby` tool
+    'rake'           => ['~> 0', '>= 0.8.4'],
+    'rubyforge'      => '~> 2',                 # for publishing gems
+    'mechanize'      => '~> 0',                 # for web automation
+    'voloko-sdoc'    => ['~> 0', '>= 0.2.10'],  # for API docs
+    'addressable'    => '~> 2',                 # for parsing URIs properly
+    'erbook'         => '~> 7',                 # for generating user manual
+    'babelfish'      => '~> 0',                 # for language translation
+    'relevance-rcov' => nil,                    # for code coverage statistics
+    'flay'           => nil,                    # for code quality analysis
+    'reek'           => nil,                    # for code quality analysis
+    'roodi'          => nil,                    # for code quality analysis
+    'ZenTest'        => '~> 4',                 # for the `multiruby` tool
   }
